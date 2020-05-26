2c667858e8eda7ebc21b7c9b4917bd74de4bf559
Aaron <64151135+aaronp18@users.noreply.github.com>
2020-05-26 19:34:55 +0100

    Added authentication to commands from get requests

--- discraft/src/main/java/com/hourglassprograms/discraft/Main.java
+++ discraft/src/main/java/com/hourglassprograms/discraft/Main.java
@@ -1,18 +1,19 @@
 package com.hourglassprograms.discraft;
 
+import java.security.MessageDigest;
+import java.math.BigInteger;
 
+import java.security.NoSuchAlgorithmException;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import express.Express;
-import express.ExpressListener;
 
 public class Main extends JavaPlugin {
     // Use to get jar with all dependencies
@@ -53,12 +54,11 @@ public class Main extends JavaPlugin {
                 // Define Root Greeting
                 get("/", (req, res) -> res.send("Welcome to DisCraft"));
                 get("/run/:command/:hash", (req, res) -> {
-                    if(checkHash(req.getParam("command"), req.getParam("hash"))){
+                    if (checkHash(req.getParam("command"), req.getParam("hash"))) {
                         runCommand(req.getParam("command"));
                         res.send("200 - " + req.getParam("command") + " was ran successfully");
-                    }
-                    else {
-                        res.send("401 - Hash was wrong. Perhaps the auth key is incorrect?");
+                    } else {
+                        res.send("401 - Hash  didnt match. Perhaps the auth key is incorrect?");
                     }
                 });
                 // get("/*", (req, res) -> res.send("404 - Wrong address"));
@@ -83,17 +83,44 @@ public class Main extends JavaPlugin {
         });
 
     }
+
     public boolean checkHash(String command, String hash) {
-        // String authkey = getConfig().getString("authkey");
-        // String message = command + authkey;
-        // getLogger().info("Message: " + message);
-        // byte[] bytesOfMessage = message.getBytes("UTF-8");
+        String authkey = getConfig().getString("authkey");
+        String message = command + authkey;
+        getLogger().info("Message: " + message);
+        try {
 
-        // MessageDigest md = MessageDigest.getInstance("MD5");
-        // byte[] thedigest = md.digest(bytesOfMessage);
-        // getLogger().info(thedigest.toString());
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
 
-        return true;
+            // digest() method is called to calculate message digest
+            // of an input digest() return array of byte
+            byte[] messageDigest = md.digest(message.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            getLogger().info(hashtext + " " + hash);
+
+            if (hashtext.equals(hash)) {
+                getLogger().info("They match");
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
